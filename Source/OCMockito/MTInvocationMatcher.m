40dfecf91438ba198e5a6b2087d704745d7f8160
Jon Reid <jon.reid@mac.com>
2011-04-17 17:19:04 -0700

    Extract method refactorings

--- Source/OCMockito/MTInvocationMatcher.m
+++ Source/OCMockito/MTInvocationMatcher.m
@@ -18,6 +18,8 @@
 @property(nonatomic, retain) NSInvocation *expected;
 @property(nonatomic, retain) NSMutableArray *argumentMatchers;
 @property(nonatomic, assign) NSUInteger numberOfArguments;
+- (BOOL)objectArgumentMismatchInInvocation:(NSInvocation *)actual atIndex:(NSUInteger)index;
+- (BOOL)charArgumentMismatchInInvocation:(NSInvocation *)actual atIndex:(NSUInteger)index;
 @end
 
 
@@ -86,37 +88,47 @@
 
     for (NSUInteger argumentIndex = 2; argumentIndex < numberOfArguments; ++argumentIndex)
     {
-        id <HCMatcher> matcher = [argumentMatchers objectAtIndex:argumentIndex];
         const char *argumentType = [methodSignature getArgumentTypeAtIndex:argumentIndex];
         if (strcmp(argumentType, @encode(id)) == 0)
         {
-            id actualArgument;
-            [actual getArgument:&actualArgument atIndex:argumentIndex];
-            
-            if (![matcher matches:actualArgument])
+            if ([self objectArgumentMismatchInInvocation:actual atIndex:argumentIndex])
                 return NO;
         }
         else if (strcmp(argumentType, @encode(char)) == 0)
         {
-            char actualArgument;
-            [actual getArgument:&actualArgument atIndex:argumentIndex];
-            
-            if ([matcher isEqual:[NSNull null]])
-            {
-                char expectedArgument;
-                [expected getArgument:&expectedArgument atIndex:argumentIndex];
-                if (expectedArgument != actualArgument)
-                    return NO;
-            }
-            else
-            {
-                if (![matcher matches:[NSNumber numberWithChar:actualArgument]])
-                    return NO;
-            }
+            if ([self charArgumentMismatchInInvocation:actual atIndex:argumentIndex])
+                return NO;
         }
     }
     
     return YES;
+}
+
+
+- (BOOL)objectArgumentMismatchInInvocation:(NSInvocation *)actual atIndex:(NSUInteger)index
+{
+    id actualArgument;
+    [actual getArgument:&actualArgument atIndex:index];
+    
+    id <HCMatcher> matcher = [argumentMatchers objectAtIndex:index];
+    return ![matcher matches:actualArgument];
+}
+
+
+- (BOOL)charArgumentMismatchInInvocation:(NSInvocation *)actual atIndex:(NSUInteger)index
+{
+    char actualArgument;
+    [actual getArgument:&actualArgument atIndex:index];
+    
+    id <HCMatcher> matcher = [argumentMatchers objectAtIndex:index];
+    if ([matcher isEqual:[NSNull null]])
+    {
+        char expectedArgument;
+        [expected getArgument:&expectedArgument atIndex:index];
+        return expectedArgument != actualArgument;
+    }
+    else
+        return ![matcher matches:[NSNumber numberWithChar:actualArgument]];
 }
 
 @end
