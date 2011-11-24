4226ad7fd3e91c49d66c377d3ebbb0d3ea3980fd
Jon Reid <jon.reid@mac.com>
2011-11-24 13:17:45 -0800

    More examples

--- Examples/MacExample/Example.m
+++ Examples/MacExample/Example.m
@@ -1,7 +1,7 @@
 #import <SenTestingKit/SenTestingKit.h>
 
-//#define HC_SHORTHAND
-//#import <OCHamcrest/OCHamcrest.h>
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
 
 #define MOCKITO_SHORTHAND
 #import <OCMockito/OCMockito.h>
@@ -11,13 +11,50 @@
 
 @implementation Example
 
-- (void)testExample
+- (void)testSimpleVerify
 {
     NSMutableArray *mockArray = mock([NSMutableArray class]);
     
-//    [mockArray removeAllObjects];
+    [mockArray removeAllObjects];
     
     [verify(mockArray) removeAllObjects];
+}
+
+- (void)testVerifyWithArgument
+{
+    NSMutableArray *mockArray = mock([NSMutableArray class]);
+    
+    [mockArray removeObject:@"This is a test"];
+    
+    [verify(mockArray) removeObject:@"This is a test"];
+}
+
+- (void)testVerifyWithMatcher
+{
+    NSMutableArray *mockArray = mock([NSMutableArray class]);
+    
+    [mockArray removeObject:@"This is a test"];
+    
+    [verify(mockArray) removeObject:startsWith(@"This is")];
+}
+
+- (void)testVerifyWithPrimitive
+{
+    NSMutableArray *mockArray = mock([NSMutableArray class]);
+    
+    [mockArray removeObjectAtIndex:2];
+    
+    [verify(mockArray) removeObjectAtIndex:2];
+}
+
+- (void)testVerifyWithMatcherForPrimitive
+{
+    NSMutableArray *mockArray = mock([NSMutableArray class]);
+    
+    [mockArray removeObjectAtIndex:2];
+    
+    [[verify(mockArray) withMatcher:greaterThan([NSNumber numberWithInt:1]) forArgument:0]
+     removeObjectAtIndex:0];    // The 0 is a placeholder, replaced by the matcher
 }
 
 @end
--- Examples/iOSExample/Example.m
+++ Examples/iOSExample/Example.m
@@ -1,7 +1,7 @@
 #import <SenTestingKit/SenTestingKit.h>
 
-//#define HC_SHORTHAND
-//#import <OCHamcrestIOS/OCHamcrestIOS.h>
+#define HC_SHORTHAND
+#import <OCHamcrestIOS/OCHamcrestIOS.h>
 
 #define MOCKITO_SHORTHAND
 #import <OCMockitoIOS/OCMockitoIOS.h>
@@ -11,13 +11,50 @@
 
 @implementation Example
 
-- (void)testExample
+- (void)testSimpleVerify
 {
     NSMutableArray *mockArray = mock([NSMutableArray class]);
     
-//    [mockArray removeAllObjects];
+    [mockArray removeAllObjects];
     
     [verify(mockArray) removeAllObjects];
+}
+
+- (void)testVerifyWithArgument
+{
+    NSMutableArray *mockArray = mock([NSMutableArray class]);
+    
+    [mockArray removeObject:@"This is a test"];
+    
+    [verify(mockArray) removeObject:@"This is a test"];
+}
+
+- (void)testVerifyWithMatcher
+{
+    NSMutableArray *mockArray = mock([NSMutableArray class]);
+    
+    [mockArray removeObject:@"This is a test"];
+    
+    [verify(mockArray) removeObject:startsWith(@"This is")];
+}
+
+- (void)testVerifyWithPrimitive
+{
+    NSMutableArray *mockArray = mock([NSMutableArray class]);
+    
+    [mockArray removeObjectAtIndex:2];
+    
+    [verify(mockArray) removeObjectAtIndex:2];
+}
+
+- (void)testVerifyWithMatcherForPrimitive
+{
+    NSMutableArray *mockArray = mock([NSMutableArray class]);
+    
+    [mockArray removeObjectAtIndex:2];
+    
+    [[verify(mockArray) withMatcher:greaterThan([NSNumber numberWithInt:1]) forArgument:0]
+     removeObjectAtIndex:0];    // The 0 is a placeholder, replaced by the matcher
 }
 
 @end
