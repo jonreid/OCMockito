0721186d2a85682d8875a33edd9a608946c0ff83
Jon Reid <jon.reid@mac.com>
2013-08-27 22:03:37 -0700

    Use OCHamcrest's new HCTestFailureHandler to report test failures.

--- Source/OCMockito/MKTTestLocation.m
+++ Source/OCMockito/MKTTestLocation.m
@@ -8,25 +8,21 @@
 
 #import "MKTTestLocation.h"
 
-#import "MKTException.h"
-
-
-// As of 2010-09-09, the iPhone simulator has a bug where you can't catch exceptions when they are
-// thrown across NSInvocation boundaries. (See http://openradar.appspot.com/8081169 ) So instead of
-// using an NSInvocation to call -failWithException: without linking in SenTestingKit, we simply
-// pretend it exists on NSObject.
-@interface NSObject (MTExceptionBugHack)
-- (void)failWithException:(NSException *)exception;
-@end
+#if TARGET_OS_MAC
+    #import <OCHamcrest/OCHamcrest.h>
+#else
+    #import <OCHamcrestIOS/OCHamcrestIOS.h>
+#endif
 
 
 void MKTFailTest(id testCase, const char *fileName, int lineNumber, NSString *description)
 {
-    NSString *theFileName = @(fileName);
-    NSException *failure = [MKTException failureInFile:theFileName
-                                                atLine:lineNumber
-                                                reason:description];
-    [testCase failWithException:failure];
+    HCTestFailure *failure = [[HCTestFailure alloc] initWithTestCase:testCase
+                                                            fileName:[NSString stringWithUTF8String:fileName]
+                                                          lineNumber:(NSUInteger)lineNumber
+                                                              reason:description];
+    id <HCTestFailureHandler> chain = HC_testFailureHandlerChain();
+    [chain handleFailure:failure];
 }
 
 void MKTFailTestLocation(MKTTestLocation testLocation, NSString *description)
