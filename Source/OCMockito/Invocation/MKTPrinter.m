e983e137313ca72714eecb2d973ef429c35b0dde
Jon Reid <jon@qualitycoding.org>
2015-12-12 22:22:22 -0800

    Refactor

--- Source/OCMockito/Invocation/MKTPrinter.m
+++ Source/OCMockito/Invocation/MKTPrinter.m
@@ -10,16 +10,29 @@
 
 - (NSString *)printInvocation:(NSInvocation *)invocation
 {
-    NSString *selector = NSStringFromSelector(invocation.selector);
-    if (invocation.methodSignature.numberOfArguments == 2) // Indices 0 and 1 are self and _cmd
-        return selector;
-    
+    if ([self invocationHasNoArguments:invocation])
+        return [self printInvocationWithNoArguments:invocation];
+    return [self printInvocationWithArguments:invocation];
+}
+
+- (BOOL)invocationHasNoArguments:(NSInvocation *)invocation
+{
+    return invocation.methodSignature.numberOfArguments == 2; // Indices 0 and 1 are self and _cmd
+}
+
+- (NSString *)printInvocationWithNoArguments:(NSInvocation *)invocation
+{
+    return NSStringFromSelector(invocation.selector);
+}
+
+- (NSString *)printInvocationWithArguments:(NSInvocation *)invocation
+{
     NSArray *arguments = [invocation mkt_arguments];
-    NSArray *selectorParts = [selector componentsSeparatedByString:@":"];
+    NSArray *selectorParts = [NSStringFromSelector(invocation.selector) componentsSeparatedByString:@":"];
     NSMutableString *result = [[NSMutableString alloc] init];
     for (NSUInteger index = 0; index < arguments.count; ++index)
     {
-        const char *argType = [invocation.methodSignature getArgumentTypeAtIndex:index+2];
+        const char *argType = [invocation.methodSignature getArgumentTypeAtIndex:index + 2];
         [result appendFormat:@" %@:%@", selectorParts[index], [self printArgument:arguments[index] type:argType]];
     }
     return [result substringFromIndex:1]; // Remove first space
