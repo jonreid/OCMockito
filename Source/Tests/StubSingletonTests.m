3beaa27dae70ef8d05a25622aaae3b4d08ce6683
Jon Reid <jon@qualitycoding.org>
2016-09-04 09:11:35 -0700

    Just use user defaults for singleton tests

--- Source/Tests/StubSingletonTests.m
+++ Source/Tests/StubSingletonTests.m
@@ -8,95 +8,31 @@
 #import <XCTest/XCTest.h>
 
 
-@interface ClassWithSingleton : NSObject
-@end
-
-
-@implementation ClassWithSingleton
-
-+ (id)singletonMethod
-{
-    static ClassWithSingleton *singleton = nil;
-    if (!singleton)
-        singleton = [[ClassWithSingleton alloc] init];
-    return singleton;
-}
-
-@end
-
-
 @interface StubSingletonTests : XCTestCase
 @end
 
 @implementation StubSingletonTests
 
-- (void)testUnstubbedSingleton_ShouldReturnExpectedInstance
-{
-    ClassWithSingleton *instance = [ClassWithSingleton singletonMethod];
-    
-    assertThat(instance, is(instanceOf([ClassWithSingleton class])));
-}
-
 - (void)testStubbedSingleton_ShouldReturnGivenObject
 {
-    __strong Class myMockClass = mockClass([ClassWithSingleton class]);
-    stubSingleton(myMockClass, singletonMethod);
-    
-    [given([myMockClass singletonMethod]) willReturn:@"STUBBED"];
-    
-    assertThat([ClassWithSingleton singletonMethod], is(@"STUBBED"));
-}
-
-- (void)testStubbedSingletonOnExistingClass_ShouldReturnGivenObject
-{
-    __strong Class myMockClass = mockClass([ClassWithSingleton class]);
-    Class userDefaultsClass = mockClass([NSUserDefaults class]);
-    
-    stubSingleton(userDefaultsClass, standardUserDefaults);
-    
-    [given([userDefaultsClass standardUserDefaults]) willReturn:@"STUBBED"];
+    __strong Class mockUserDefaultsClass = mockClass([NSUserDefaults class]);
+    stubSingleton(mockUserDefaultsClass, standardUserDefaults);
+    [given([mockUserDefaultsClass standardUserDefaults]) willReturn:@"STUBBED"];
     
     assertThat([NSUserDefaults standardUserDefaults], is(@"STUBBED"));
 }
 
-- (void)testStubbedSingleton_LastSingletonStubTakesPrecedence
+- (void)testStubbedSingleton_WithMultipleStubs_ShouldGivePrecedenceToLastStub
 {
-    __strong Class myMockClass = mockClass([ClassWithSingleton class]);
-    stubSingleton(myMockClass, singletonMethod);
+    __strong Class firstMockClass = mockClass([NSUserDefaults class]);
+    stubSingleton(firstMockClass, standardUserDefaults);
+    [given([firstMockClass standardUserDefaults]) willReturn:@"STUBBED"];
     
-    [given([myMockClass singletonMethod]) willReturn:@"STUBBED"];
+    __strong Class secondMockClass = mockClass([NSUserDefaults class]);
+    stubSingleton(secondMockClass, standardUserDefaults);
+    [given([secondMockClass standardUserDefaults]) willReturn:@"STUBBED2"];
     
-    Class myNewMockClass = mockClass([ClassWithSingleton class]);
-    
-    stubSingleton(myNewMockClass, singletonMethod);
-    
-    [given([myNewMockClass singletonMethod]) willReturn:@"STUBBED2"];
-    
-    assertThat([ClassWithSingleton singletonMethod], is(@"STUBBED2"));
+    assertThat([NSUserDefaults standardUserDefaults], is(@"STUBBED2"));
 }
-
-//- (void)testStubbedSingleton_ValidUnswizzle
-//{
-//    stubSingleton(myMockClass, singletonMethod);
-//
-//    [given([myMockClass singletonMethod]) willReturn:@"STUBBED"];
-//
-//    MKTClassObjectMock *mock = (MKTClassObjectMock *)myMockClass;
-//    [mock unswizzleSingletonAtSelector:@selector(singletonMethod)];
-//
-//    assertThat([ClassMethodsReturningObject singletonMethod], isNot(@"STUBBED"));
-//}
-//
-//- (void)testStubbedSingleton_InvalidUnswizzling
-//{
-//    MKTClassObjectMock *mock = (MKTClassObjectMock *)myMockClass;
-//
-//    [mock unswizzleSingletonAtSelector:@selector(standardUserDefaults)];
-//}
-//
-//- (void)testNoStubbedSingleton_ReturnsNil
-//{
-//    assertThat([MKTClassObjectMock mockSingleton], equalTo(nil));
-//}
 
 @end
