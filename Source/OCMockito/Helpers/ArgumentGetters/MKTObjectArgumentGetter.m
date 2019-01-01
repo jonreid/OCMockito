b090248486b7f3bf503d150b34017c1f1a8673ec
Jon Reid <jon@qualitycoding.org>
2019-01-01 09:13:01 -0800

    Bump copyright to 2019

--- Source/OCMockito/Helpers/ArgumentGetters/MKTObjectArgumentGetter.m
+++ Source/OCMockito/Helpers/ArgumentGetters/MKTObjectArgumentGetter.m
@@ -1,5 +1,5 @@
 //  OCMockito by Jon Reid, https://qualitycoding.org/
-//  Copyright 2018 Jonathan M. Reid. See LICENSE.txt
+//  Copyright 2019 Jonathan M. Reid. See LICENSE.txt
 
 #import "MKTObjectArgumentGetter.h"
 
