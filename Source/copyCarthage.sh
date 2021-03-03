986e8eebd6ef4581fac11137e6d997a741acb2d1
Jon Reid <jon@qualitycoding.org>
2021-03-02 22:12:46 -0800

    Make copy carthage script external across targets

---
+++ Source/copyCarthage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CARTHAGE_BUILD=../../../Build/OCHamcrest.xcframework
+LOCAL_FRAMEWORK=../Frameworks/OCHamcrest-8.0.0/
+
+if [ -e $CARTHAGE_BUILD ] && [ ! -e $LOCAL_FRAMEWORK/OCHamcrest.xcframework ]; then
+  echo "Copying Carthage OCHamcrest"
+  mkdir -p $LOCAL_FRAMEWORK
+  cp -pR $CARTHAGE_BUILD $LOCAL_FRAMEWORK/OCHamcrest.xcframework
+fi
