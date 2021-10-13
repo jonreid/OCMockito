a3beac32c82305c9f31db3fe3fe2d8d1fc1febee
Jon Reid <jon@qualitycoding.org>
2021-10-12 21:51:23 -0700

    d Add Swift Package Manager to Examples

---
+++ Examples/MacExample-SwiftPackageManager/Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Example",
+    products: [
+        .library(
+            name: "Example",
+            targets: ["Example"]),
+    ],
+    // begin-snippet: swiftpm-declare-dependencies
+    dependencies: [
+        .package(
+            name: "OCMockito",
+            url: "https://github.com/jonreid/OCMockito",
+            .branch("main")
+//            .upToNextMajor(from: "7.0.0")
+        ),
+    ],
+    // end-snippet
+    targets: [
+        .target(
+            name: "Example",
+            dependencies: [],
+            publicHeadersPath: "./"
+        ),
+        // begin-snippet: swiftpm-use-dependencies
+        .testTarget(
+            name: "ExampleTests",
+            dependencies: [
+                "Example",
+                "OCMockito",
+            ]
+        ),
+        // end-snippet
+    ]
+)
---
+++ Examples/MacExample-SwiftPackageManager/README.md
@@ -0,0 +1,1 @@
+`swift test` to run unit tests. Try changing some tests to fail.
