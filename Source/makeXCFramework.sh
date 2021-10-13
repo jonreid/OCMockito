ccddb1d5b5ab1da0601c1ee8888461f09eb5539b
Jon Reid <jon@qualitycoding.org>
2021-10-12 21:33:03 -0700

    e Add Catalyst support to makeXCFramework.sh

--- Source/makeXCFramework.sh
+++ Source/makeXCFramework.sh
@@ -3,6 +3,7 @@
 FRAMEWORK_NAME="OCMockito"
 
 MACOS_ARCHIVE_PATH="./build/archives/macos.xcarchive"
+CATALYST_ARCHIVE_PATH="./build/archives/mac_catalyst.xcarchive"
 IOS_ARCHIVE_PATH="./build/archives/ios.xcarchive"
 IOS_SIMULATOR_ARCHIVE_PATH="./build/archives/ios_sim.xcarchive"
 TV_ARCHIVE_PATH="./build/archives/tv.xcarchive"
@@ -11,6 +12,7 @@ WATCH_ARCHIVE_PATH="./build/archives/watch.xcarchive"
 WATCH_SIMULATOR_ARCHIVE_PATH="./build/archives/watch_sim.xcarchive"
 
 xcodebuild archive -scheme ${FRAMEWORK_NAME} -archivePath ${MACOS_ARCHIVE_PATH} -sdk macosx SKIP_INSTALL=NO
+xcodebuild archive -scheme ${FRAMEWORK_NAME}-iOS -archivePath ${CATALYST_ARCHIVE_PATH} -destination 'platform=macOS,variant=Mac Catalyst' SKIP_INSTALL=NO
 xcodebuild archive -scheme ${FRAMEWORK_NAME}-iOS -archivePath ${IOS_ARCHIVE_PATH} -sdk iphoneos SKIP_INSTALL=NO
 xcodebuild archive -scheme ${FRAMEWORK_NAME}-iOS -archivePath ${IOS_SIMULATOR_ARCHIVE_PATH} -sdk iphonesimulator SKIP_INSTALL=NO
 xcodebuild archive -scheme ${FRAMEWORK_NAME}-tvOS -archivePath ${TV_ARCHIVE_PATH} -sdk appletvos SKIP_INSTALL=NO
@@ -20,6 +22,7 @@ xcodebuild archive -scheme ${FRAMEWORK_NAME}-watchOS -archivePath ${WATCH_SIMULA
 
 xcodebuild -create-xcframework \
   -framework ${MACOS_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+  -framework ${CATALYST_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
   -framework ${IOS_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
   -framework ${IOS_SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
   -framework ${TV_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
