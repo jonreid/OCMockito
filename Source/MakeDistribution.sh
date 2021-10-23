ca45a6547ad9cd20e19c6d8dea2aa3951399f77f
Jon Reid <jon@qualitycoding.org>
2021-10-23 10:55:50 -0700

    e Fix script warning

--- Source/MakeDistribution.sh
+++ Source/MakeDistribution.sh
@@ -33,12 +33,12 @@ cp "${PROJECTROOT}/CHANGELOG.md" "${DISTPATH}"
 cp "${PROJECTROOT}/LICENSE.txt" "${DISTPATH}"
 cp -R "${PROJECTROOT}/Examples" "${DISTPATH}"
 
-find "${DISTPATH}/Examples" -type d \( -name 'build' -or -name 'xcuserdata' -or -name '.svn' -or -name '.git' \) | while read DIR
+find "${DISTPATH}/Examples" -type d \( -name 'build' -or -name 'xcuserdata' -or -name '.svn' -or -name '.git' \) | while read -r DIR
 do
     rm -R "${DIR}";
 done
 
-find "${DISTPATH}/Examples" -type f \( -name '*.pbxuser' -or -name '*.perspectivev3' -or -name '*.mode1v3' -or -name '.DS_Store' -or -name '.gitignore' \) | while read FILE
+find "${DISTPATH}/Examples" -type f \( -name '*.pbxuser' -or -name '*.perspectivev3' -or -name '*.mode1v3' -or -name '.DS_Store' -or -name '.gitignore' \) | while read -r FILE
 do
     rm "${FILE}";
 done
