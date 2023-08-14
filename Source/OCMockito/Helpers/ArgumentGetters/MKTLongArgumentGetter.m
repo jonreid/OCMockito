b91b6c39fb4ef0168d09d3a7b44ce3eb67b4fb02
Jon Reid <jon@qualitycoding.org>
2023-08-13 21:16:16 -0700

    r - restore 'year of first publication' in copyright notice

--- Source/OCMockito/Helpers/ArgumentGetters/MKTLongArgumentGetter.m
+++ Source/OCMockito/Helpers/ArgumentGetters/MKTLongArgumentGetter.m
@@ -1,5 +1,5 @@
 // OCMockito by Jon Reid, https://qualitycoding.org
-// Copyright 2023 Jonathan M. Reid. https://github.com/jonreid/OCMockito/blob/main/LICENSE.txt
+// Copyright 2011 Jonathan M. Reid. https://github.com/jonreid/OCMockito/blob/main/LICENSE.txt
 // SPDX-License-Identifier: MIT
 
 #import "MKTLongArgumentGetter.h"
