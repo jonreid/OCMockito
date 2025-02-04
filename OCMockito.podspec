c4a796fb3e2be106ef2c4bbbacc23ff8089027c0
Jens Nerup <jens@makecph.com>
2025-02-04 18:54:48 +0100

    Remove multiple Xcode targets Updated the Xcode project to contain only one library target and a test target. The library target has been updated to support multiple platforms (including visionOS) and multiple TARGETED_DEVICE_FAMILY. All configuration related to the target platforms have been extracted into XcodeTargets.xcconfig including the deployment targets. Additionally the Cartfile, Podspec and makeXCFramework.sh have been updated to reflect these changes.

--- OCMockito.podspec
+++ OCMockito.podspec
@@ -25,6 +25,7 @@ Pod::Spec.new do |s|
   s.ios.deployment_target = '12.0'
   s.tvos.deployment_target = '12.0'
   s.watchos.deployment_target = '4.0'
+  s.visionos.deployment_target = '1.0'
   s.source   = { :git => 'https://github.com/jonreid/OCMockito.git', :tag => 'v7.0.2' }
   s.source_files = 'Source/OCMockito/**/*.{h,m}', 'Source/ThirdParty/**/*.{h,m}'
   s.public_header_files = 'Source/OCMockito/Core/MKTNonObjectArgumentMatching.h',  'Source/OCMockito/Core/OCMockito.h',  'Source/OCMockito/Invocation/NSInvocation+OCMockito.h',  'Source/OCMockito/Mocking/MKTBaseMockObject.h',  'Source/OCMockito/Mocking/MKTClassObjectMock.h',  'Source/OCMockito/Mocking/MKTObjectAndProtocolMock.h',  'Source/OCMockito/Mocking/MKTObjectMock.h',  'Source/OCMockito/Mocking/MKTProtocolMock.h',  'Source/OCMockito/Stubbing/MKTOngoingStubbing.h'
--- Source/makeXCFramework.sh
+++ Source/makeXCFramework.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# Use this script to create an XCFramework bundle that includes the frameworks necessary to build
+# for multiple platforms (iOS, macOS, visionOS, tvOS, watchOS), including Simulator builds. Please
+# beware that the script will fail when creating archives for xros/xrsimulator on computers running
+# macOS on Intel architecture since the `-destination generic/platform=visionOS ...` is missing on
+# that architecture.
 
+XCODE_PROJECT_PATH="OCMockito.xcodeproj"
 FRAMEWORK_NAME="OCMockito"
 
 MACOS_ARCHIVE_PATH="./build/archives/macos.xcarchive"
@@ -10,23 +16,29 @@ TV_ARCHIVE_PATH="./build/archives/tv.xcarchive"
 TV_SIMULATOR_ARCHIVE_PATH="./build/archives/tv_sim.xcarchive"
 WATCH_ARCHIVE_PATH="./build/archives/watch.xcarchive"
 WATCH_SIMULATOR_ARCHIVE_PATH="./build/archives/watch_sim.xcarchive"
+XR_ARCHIVE_PATH="./build/archives/xr.xcarchive"
+XR_SIMULATOR_ARCHIVE_PATH="./build/archives/xr_sim.xcarchive"
 
-xcodebuild archive -scheme ${FRAMEWORK_NAME} -archivePath ${MACOS_ARCHIVE_PATH} -sdk macosx SKIP_INSTALL=NO
-xcodebuild archive -scheme ${FRAMEWORK_NAME}-iOS -archivePath ${CATALYST_ARCHIVE_PATH} -destination 'platform=macOS,variant=Mac Catalyst' SKIP_INSTALL=NO
-xcodebuild archive -scheme ${FRAMEWORK_NAME}-iOS -archivePath ${IOS_ARCHIVE_PATH} -sdk iphoneos SKIP_INSTALL=NO
-xcodebuild archive -scheme ${FRAMEWORK_NAME}-iOS -archivePath ${IOS_SIMULATOR_ARCHIVE_PATH} -sdk iphonesimulator SKIP_INSTALL=NO
-xcodebuild archive -scheme ${FRAMEWORK_NAME}-tvOS -archivePath ${TV_ARCHIVE_PATH} -sdk appletvos SKIP_INSTALL=NO
-xcodebuild archive -scheme ${FRAMEWORK_NAME}-tvOS -archivePath ${TV_SIMULATOR_ARCHIVE_PATH} -sdk appletvsimulator SKIP_INSTALL=NO
-xcodebuild archive -scheme ${FRAMEWORK_NAME}-watchOS -archivePath ${WATCH_ARCHIVE_PATH} -sdk watchos SKIP_INSTALL=NO
-xcodebuild archive -scheme ${FRAMEWORK_NAME}-watchOS -archivePath ${WATCH_SIMULATOR_ARCHIVE_PATH} -sdk watchsimulator SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${MACOS_ARCHIVE_PATH} -sdk macosx SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${CATALYST_ARCHIVE_PATH} -destination 'generic/platform=macOS,variant=Mac Catalyst' SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${IOS_ARCHIVE_PATH} -sdk iphoneos -destination "generic/platform=iOS" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${IOS_SIMULATOR_ARCHIVE_PATH} -sdk iphonesimulator -destination "generic/platform=iOS Simulator" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${TV_ARCHIVE_PATH} -sdk appletvos -destination "generic/platform=tvOS" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${TV_SIMULATOR_ARCHIVE_PATH} -sdk appletvsimulator -destination "generic/platform=tvOS Simulator" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${WATCH_ARCHIVE_PATH} -sdk watchos -destination "generic/platform=watchOS" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${WATCH_SIMULATOR_ARCHIVE_PATH} -sdk watchsimulator -destination "generic/platform=watchOS Simulator" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${XR_ARCHIVE_PATH} -sdk xros -destination "generic/platform=visionOS" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${XR_SIMULATOR_ARCHIVE_PATH} -sdk xrsimulator -destination "generic/platform=visionOS Simulator" SKIP_INSTALL=NO
 
 xcodebuild -create-xcframework \
-  -framework ${MACOS_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
-  -framework ${CATALYST_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
-  -framework ${IOS_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
-  -framework ${IOS_SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
-  -framework ${TV_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
-  -framework ${TV_SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
-  -framework ${WATCH_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
-  -framework ${WATCH_SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+  -archive ${MACOS_ARCHIVE_PATH} -framework ${FRAMEWORK_NAME}.framework \
+  -archive ${CATALYST_ARCHIVE_PATH} -framework ${FRAMEWORK_NAME}.framework \
+  -archive ${IOS_ARCHIVE_PATH} -framework ${FRAMEWORK_NAME}.framework \
+  -archive ${IOS_SIMULATOR_ARCHIVE_PATH} -framework ${FRAMEWORK_NAME}.framework \
+  -archive ${TV_ARCHIVE_PATH} -framework ${FRAMEWORK_NAME}.framework \
+  -archive ${TV_SIMULATOR_ARCHIVE_PATH} -framework ${FRAMEWORK_NAME}.framework \
+  -archive ${WATCH_ARCHIVE_PATH} -framework ${FRAMEWORK_NAME}.framework \
+  -archive ${WATCH_SIMULATOR_ARCHIVE_PATH} -framework ${FRAMEWORK_NAME}.framework \
+  -archive ${XR_ARCHIVE_PATH} -framework ${FRAMEWORK_NAME}.framework \
+  -archive ${XR_SIMULATOR_ARCHIVE_PATH} -framework ${FRAMEWORK_NAME}.framework \
   -output "./build/${FRAMEWORK_NAME}.xcframework"
