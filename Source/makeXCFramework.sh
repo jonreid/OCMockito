5f367d3cc84af735d0cd2abb33ef4cde360c8517
Jens Nerup <jens@makecph.com>
2025-02-11 21:14:48 +0100

    Update xcframework script

--- Source/makeXCFramework.sh
+++ Source/makeXCFramework.sh
@@ -19,16 +19,16 @@ WATCH_SIMULATOR_ARCHIVE_PATH="./build/archives/watch_sim.xcarchive"
 XR_ARCHIVE_PATH="./build/archives/xr.xcarchive"
 XR_SIMULATOR_ARCHIVE_PATH="./build/archives/xr_sim.xcarchive"
 
-xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${MACOS_ARCHIVE_PATH} -sdk macosx SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${MACOS_ARCHIVE_PATH} -destination 'generic/platform=macOS' SKIP_INSTALL=NO
 xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${CATALYST_ARCHIVE_PATH} -destination 'generic/platform=macOS,variant=Mac Catalyst' SKIP_INSTALL=NO
-xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${IOS_ARCHIVE_PATH} -sdk iphoneos -destination "generic/platform=iOS" SKIP_INSTALL=NO
-xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${IOS_SIMULATOR_ARCHIVE_PATH} -sdk iphonesimulator -destination "generic/platform=iOS Simulator" SKIP_INSTALL=NO
-xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${TV_ARCHIVE_PATH} -sdk appletvos -destination "generic/platform=tvOS" SKIP_INSTALL=NO
-xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${TV_SIMULATOR_ARCHIVE_PATH} -sdk appletvsimulator -destination "generic/platform=tvOS Simulator" SKIP_INSTALL=NO
-xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${WATCH_ARCHIVE_PATH} -sdk watchos -destination "generic/platform=watchOS" SKIP_INSTALL=NO
-xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${WATCH_SIMULATOR_ARCHIVE_PATH} -sdk watchsimulator -destination "generic/platform=watchOS Simulator" SKIP_INSTALL=NO
-xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${XR_ARCHIVE_PATH} -sdk xros -destination "generic/platform=visionOS" SKIP_INSTALL=NO
-xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${XR_SIMULATOR_ARCHIVE_PATH} -sdk xrsimulator -destination "generic/platform=visionOS Simulator" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${IOS_ARCHIVE_PATH} -destination "generic/platform=iOS" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${IOS_SIMULATOR_ARCHIVE_PATH} -destination "generic/platform=iOS Simulator" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${TV_ARCHIVE_PATH} -destination "generic/platform=tvOS" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${TV_SIMULATOR_ARCHIVE_PATH} -destination "generic/platform=tvOS Simulator" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${WATCH_ARCHIVE_PATH} -destination "generic/platform=watchOS" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${WATCH_SIMULATOR_ARCHIVE_PATH} -destination "generic/platform=watchOS Simulator" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${XR_ARCHIVE_PATH} -destination "generic/platform=visionOS" SKIP_INSTALL=NO
+xcodebuild archive -project ${XCODE_PROJECT_PATH} -scheme ${FRAMEWORK_NAME} -archivePath ${XR_SIMULATOR_ARCHIVE_PATH} -destination "generic/platform=visionOS Simulator" SKIP_INSTALL=NO
 
 xcodebuild -create-xcframework \
   -archive ${MACOS_ARCHIVE_PATH} -framework ${FRAMEWORK_NAME}.framework \
